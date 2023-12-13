da217ccb439de87a0aacf6c2cf850ba424bf8802
xuyajie9 <xuyajie9@jd.com>
2023-12-13 10:59:59 +0800

    feat: 完成测试

--- src/index.js
+++ src/index.js
@@ -5,23 +5,31 @@
  * @date 2023-12-08 15:10:04
  */
 
+// SDK
+const MAPS_ARR_SDK = [
+  // 百度MAP_SDK
+  '//api.map.baidu.com',
 
-const MAPS_ARR = [
-  // 腾讯map
-  'apikey.map.qq.com',
-  'confinfo.map.qq.com',
-  'overseactrl.map.qq.com',
+  // 腾讯MAP_SDK
+  '//map.qq.com/api/gljs'
 
-  // 百度map
+  // 高德MAP_SDK 【高德无跨域问题无需处理】
+  // "//webapi.amap.com/maps",
+];
+
+// JSONP
+const MAPS_ARR_JSONP = [
+  // 百度MAP
   'map.baidu.com',
   'dlswbr.baidu.com',
   'hm.baidu.com',
+  'maponline0.bdimg.com',
 
-  // 高德map
-  'webapi.amap.com',
-  'a.amap.com'
-]
-
+  // 腾讯MAP
+  'apikey.map.qq.com',
+  'confinfo.map.qq.com',
+  'overseactrl.map.qq.com'
+];
 
 /**
  * 如果函数返回 `true` 则忽略 script 和 link 标签的创建
@@ -29,7 +37,7 @@ const MAPS_ARR = [
  * @returns boolean
  */
 function MapExcludeCheckerHandler(url) {
-  return MAPS_ARR.some((item) => url.includes(item));
+  return MAPS_ARR_SDK.some((item) => url.includes(item));
 }
 
 /**
@@ -38,12 +46,12 @@ function MapExcludeCheckerHandler(url) {
  * @returns boolean
  */
 function MapIgnoreCheckerHandler(url) {
-  return MAPS_ARR.some((item) => url.includes(item));
+  return [...MAPS_ARR_SDK, ...MAPS_ARR_JSONP].some((item) => url.includes(item));
 }
 
 const microPluginMap = {
   // 可选，如果函数返回 `true` 则忽略 script 和 link 标签的创建
-  excludeChecker: MapExcludeCheckerHandler ,
+  excludeChecker: MapExcludeCheckerHandler,
   // 可选，如果函数返回 `true` ，则 micro-app 不会处理它，元素将原封不动进行渲染
   ignoreChecker: MapIgnoreCheckerHandler
 };
